75ad6b9e9e91414e15d0a203cd28074ac08a88aa
= <=>
2020-10-28 10:00:35 +0100

    procedural texture

--- src/shaders/blob/fragment.glsl
+++ src/shaders/blob/fragment.glsl
@@ -1,6 +1,5 @@
-precision highp float;
+precision mediump float;
 
-uniform sampler2D uTexture;
 uniform vec3 uColor1;
 uniform vec3 uColor2;
 
@@ -8,9 +7,19 @@ varying vec2 vUv;
 varying float vColorMix;
 varying float vAlpha;
 
+float circle(vec2 pos, float maskRadius, float smoothness) {
+    return 1. - smoothstep(
+        maskRadius - (maskRadius * smoothness),
+        maskRadius + (maskRadius * smoothness),
+        length(pos)
+    );
+}
+
 void main() {
-    vec4 texture = texture2D(uTexture, vUv);
-    if ( texture.a < 0.5 ) discard;
+    float radius = .3;
+    float smoothness = 0.2;
+    float alphaMask = circle(vUv - vec2(.5), radius, smoothness);
+    if ( alphaMask < 0.1 ) discard;
     vec3 color = mix(uColor1, uColor2, vColorMix);
-    gl_FragColor = vec4(texture.rgb * color, texture.a * vAlpha);
+    gl_FragColor = vec4(color, alphaMask * vAlpha);
 }
